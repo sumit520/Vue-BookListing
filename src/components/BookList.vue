22c16b24494aa796b8ec4f57e7301a4559822515
Sumit Jain <sumit_jain@live.com>
2019-03-20 15:46:34 -0400

    module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+        </ul>
     </div>
 </template>
 
@@ -9,7 +12,12 @@
         name:"BookList",
         data(){
             return{
-                title: 'All Books'
+                title: 'All Books',
+                books: [
+                    {title:'Book 1', author:'Sumit'},
+                    {title:'Book 2', author:'John'},
+                    {title:'Book 3', author:'Xyz'}
+                ]
             }
         }
     }
@@ -18,5 +26,13 @@
 <style scoped>
 h1, h2 { 
     font-weight: normal
+}
+ul{
+    list-style-type: none;
+    padding: 0;
+}
+li{
+    display: block;
+    margin: 0 10px;
 }
 </style>
